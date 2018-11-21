0d808ca3967cec9998f8e913cdd27e824c068d4d
Peter Geszten-Kovacs <szotyi@imind.eu>
2018-11-21 10:11:36 +0100

    V4 circleci.slackrequest parse

--- Cci.playground/Pages/V4.xcplaygroundpage/Contents.swift
+++ Cci.playground/Pages/V4.xcplaygroundpage/Contents.swift
@@ -287,9 +287,33 @@ extension SlackResponse {
     }
 }
 
+extension Collection {
+    subscript(safe index: Index) -> Element? {
+        guard indices.contains(index) else { return nil }
+        return self[index]
+    }
+}
+
 enum CircleCiError: Error {
     case noChannel(String)
     case unknownCommand(String)
+    case parse(String)
+}
+
+enum CircleCiJobKind: String, CaseIterable {
+    case deploy
+    case test
+}
+
+extension CircleCiJobKind {
+    var type: CircleCiJob.Type {
+        switch self {
+        case .deploy:
+            return CircleCiDeployJob.self
+        case .test:
+            return CircleCiTestJob.self
+        }
+    }
 }
 
 protocol CircleCiJob {
@@ -298,24 +322,89 @@ protocol CircleCiJob {
     var branch: String { get }
     var options: [String] { get }
     var username: String { get }
+
+    static var helpResponse: SlackResponse { get }
+    
+    static func parse(project: String, parameters: [String], options:[String], username: String) throws -> Either<SlackResponse, Self>
 }
 
-struct CircleciTestJob: CircleCiJob {
-    let name: String = "test"
+struct CircleCiTestJob: CircleCiJob {
+    let name: String = CircleCiJobKind.test.rawValue
     let project: String
     let branch: String
     let options: [String]
     let username: String
 }
 
-struct CircleciDeployJob: CircleCiJob {
-    let name: String = "deploy"
+extension CircleCiTestJob {
+    static var helpResponse: SlackResponse {
+        let text = "`test`: test a branch\n" +
+            "Usage:\n`/cci test branch [options]`\n" +
+            "  - *branch*: branch name to test\n" +
+        "  - *options*: optional fastlane options in the xyz:qwo format\n"
+        let attachment = SlackResponse.Attachment(
+            fallback: text, text: text, color: "good", mrkdwn_in: ["text"], fields: [])
+        let response = SlackResponse(response_type: .ephemeral, text: "Send commands to <https://circleci.com|CircleCI>", attachments: [attachment], mrkdwn: true)
+        return response
+    }
+
+    static func parse(project: String, parameters: [String], options:[String], username: String) throws -> Either<SlackResponse, CircleciTestJob> {
+        guard parameters.count > 0 else {
+            throw CircleCiError.parse("No branch found: (\(parameters.joined(separator: " ")))")
+        }
+        guard parameters[0] != "help" else {
+            return .left(CircleCiTestJob.helpResponse)
+        }
+        let branch = parameters[0]
+        return .right(CircleCiTestJob(project: project, branch: branch, options: options, username: username))
+
+    }
+}
+
+struct CircleCiDeployJob: CircleCiJob {
+    let name: String = CircleCiJobKind.deploy.rawValue
     let project: String
     let branch: String
     let options: [String]
     let username: String
     let type: String
-    
+}
+
+extension CircleCiDeployJob {
+    static var helpResponse: SlackResponse {
+        let text = "`deploy`: deploy a build\n" +
+            "Usage:\n`/cci deploy type [options] [branch]`\n" +
+            "  - *type*: alpha|beta|app_store\n" +
+            "  - *options*: optional fastlane options in the xyz:qwo format\n" +
+            "    (eg. emails:xy@test.com,zw@test.com groups:qa,beta-customers version:2.0.1)\n" +
+            "    (space shouldn't be in the option for now)\n" +
+            "  - *branch*: an optional branch name to deploy from\n" +
+            "  If emails and groups are both set, emails will be used\n" +
+            "  (currently available options, maybe not up to date: " +
+        "    emails, groups, use_git, version, skip_xcode_version_check)"
+        let attachment = SlackResponse.Attachment(
+            fallback: text, text: text, color: "good", mrkdwn_in: ["text"], fields: [])
+        let response = SlackResponse(response_type: .ephemeral, text: "Send commands to <https://circleci.com|CircleCI>", attachments: [attachment], mrkdwn: true)
+        return response
+    }
+
+    static func parse(project: String, parameters: [String], options: [String], username: String) throws -> Either<SlackResponse, CircleciDeployJob> {
+        if parameters.count == 1 && parameters[0] == help {
+            return .left(CircleCiDeployJob.helpResponse)
+        }
+        let types = [
+            "alpha": "dev",
+            "beta": "master",
+            "app_store": "release"]
+        if parameters.count == 0 || !types.keys.contains(parameters[0]) {
+            throw CircleCiError.parse("Unknown type: (\(parameters.joined(separator: " ")))")
+        }
+        let type = parameters[0]
+        guard let branch = parameters[safe: 1] ?? types[type] else {
+            throw CircleCiError.parse("No branch found: (\(parameters.joined(separator: " ")))")
+        }
+        return .right(CircleCiDeployJob(project: project, branch: branch, options: options, username: username, type: type))
+    }
 }
 
 struct CircleCiJobRequest: RequestModel {
@@ -338,7 +427,7 @@ extension CircleCiJobRequest {
         let text = "Help:\n- `/cci command [help]`\n" +
             "Current command\n" +
             "  - help: show this message\n" +
-            "  - .deploy: deploy a build\n" +
+            "  - deploy: deploy a build\n" +
             "  - test: test a branch\n\n" +
         "All commands have a help subcommand to show their functionality\n"
         let attachment = SlackResponse.Attachment(
@@ -366,12 +455,14 @@ extension CircleCiJobRequest {
         let options = parameters.filter(isOption)
         parameters = parameters.filter { !isOption($0) }
         
-        if command == "help" {
+        if let job = CircleCiJobKind(rawValue: command) {
+            do {
+                return try job.type.parse(project: project, parameters: parameters, options: options, username: from.user_name)
+            } catch {
+                return .left(SlackResponse.error(text: error.localizedDescription, helpResponse: job.type.helpResponse))
+            }
+        } else if command == "help" {
             return .left(helpResponse)
-        } else if command == "test" {
-            return .right(CircleCiJobRequest(job: CircleciTestJob(project: project, branch: "", options: options, username: from.user_name)))
-        } else if command == "deploy" {
-            return .right(CircleCiJobRequest(job: CircleciDeployJob(project: project, branch: "", options: options, username: from.user_name, type: "")))
         } else {
             return .left(SlackResponse.error(text: CircleCiError.unknownCommand(from.text).localizedDescription))
         }
